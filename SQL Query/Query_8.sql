a8a82e5f45bac4ca2288e6687a6e7b87995e83b8
tushar2704 <tushar.inseec@gmail.com>
2023-08-06 20:13:51 +0530

    updates

--- SQL Query/Query_8.sql
+++ SQL Query/Query_8.sql
@@ -2,3 +2,20 @@
 Query_7
 © 2023 Tushar Aggarwal. All rights reserved.
 */
+
+
+/*Query_8
+
+*/
+
+/*-purchases-
+\column_name\type\
+\purchase_id\integer\
+\user_id\integer\
+\product_id\integer\
+\quantity\integer\
+\price\float\
+\purchase_time\datetime\
+*/
+
+/*Solution*/

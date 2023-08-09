2029ba8186bed6bd16b367a738a8fe30ba8f7843
tushar2704 <tushar.inseec@gmail.com>
2023-08-09 17:09:19 +0530

    updates

---
+++ SQL Query/Query_10.sql
@@ -0,0 +1,21 @@
+/* SQL-Query @https://github.com/tushar2704/SQL-Query
+Query_7
+© 2023 Tushar Aggarwal. All rights reserved.
+*/
+
+
+/*Query_10
+
+*/
+
+/*-purchases-
+\column_name\type\
+\purchase_id\integer\
+\user_id\integer\
+\product_id\integer\
+\quantity\integer\
+\price\float\
+\purchase_time\datetime\
+*/
+
+/*Solution*/
